4703ca2ada827a2b456d7566cd94115c4d5455ca
박희찬 <phc5403@gmail.com>
2023-08-13 15:32:11 +0900

    Update Main_17406_박희찬.java

--- 0810/Main_17406_박희찬.java
+++ 0810/Main_17406_박희찬.java
@@ -1,71 +1,184 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Temp {  /////////////
-	static StringBuilder sb;
-	static StringTokenizer st;
-	static boolean[][] visited;  // bfs() 방문 처리
-	static boolean[][] isSelected; // 순열 방문 처리
-	static boolean[][] operation; // 회전 연산의 정보를 저장
-	static int[][] lst;  // 원본 배열
-	static int N, M, K;  // 행, 열, 회전 연산의 개수
-	static int res;  // Answer : 배열 A의 값 중 최솟값
-	
-	// 좌하우상
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {-1, 0, 1, 0};
 
-	public static void main(String[] args) {
-		/*
-		 * 1. 입력 받기
-		 * 2. 회전 연산을 2차원 배열에 저장
-		 * 3. 원본 배열을 복사함
-		 * 4. 복사본을 중복 순열의 경우의수 -> 그에 맞게 회전 -> 최솟값 구하기
-		 * 5. 모든 경우의 수를 끝낼때 까지 4번 반복.
-		 */
-	}
-	
-	/**
-	 * 각 계산을 마친 배열의 최솟값을 구하는 함수
-	 * @param array : 계산을 위한 원본 배열의 복사본
-	 */
-	private static void arrayCalcul(int[][] array) {
-		int sum = 0;
+public class Main { // 17406
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static int N, M, K;  // 원본배열 행, 열 / 회전 연산의 개수
+	static int r, c, s;  // 회전 연산의 세 정수
+	static int nN, mM;  // 연산에 의해 돌려야할 행, 열
+	static int[][] lst;  // 원본 배열
+	static int[][] copyLst;  // 연산을 위해 원본을 복사한 배열
+	static int[][] operation; // 회전 연산을 저장할 배열
+	static boolean[][] visited;  // 배열 bfs()회전시 방문 체크용
+	static int[] isSelected;  // 조합 : 회전 연산 방문 체크용
+	static int range;  // 한 번 회전시 내부 사각형까지 돌려야 하는 횟수
+	static int startX, startY;  // 회전 연산에 의해 돌려야할 출발점 좌표
+	static int res; // 최종 Answer
+
+	// 하우상좌(시계 방향)
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		res = Integer.MAX_VALUE;
 		
-		/*
-		 * 1. sum += array의 각 행에 있는 모든 수의 합.
-		 * 2. Math.min(res, sum) -> sum과 비교하여 최솟값으로 res를 갱신
-		 */
-		res = Math.min(res, sum);
-	}
-	
-	/**
-	 * 원본 배열의 복사본을 입력으로 받은 회전 연산의 범위대로
-	 * 시계 방향으로 회전하는 함수 
-	 * @param r : 회전 연산을 위한 회전 범위가 조정 될 행 좌표
-	 * @param s : 회전 연산을 위한 회전 범위가 조정 될 열 좌표
-	 * @param x : 출발점의 행 좌표
-	 * @param y : 출발점의 열 좌표
-	 * @param array : 원본 배열 복사본
-	 */
-	private static void rotate(int r, int s, int x, int y, int[][] array) {
-		// 배열 돌리기 1(반시계 회전)을 수정해 시계 방향 회전을 구현(현재 실패)
-	}
-	
-	/**
-	 * 회전 연산의 순서의 종류를 정하기 위한 중복 순열을 구하는 함수
-	 * @param cnt : 현재 선택된 숫자의 수
-	 * @param isSelected : 방문 처리용
-	 */
-	private static void perm(int cnt, int[][] isSelected) {
-		// 회전 연산의 경우의 수가 나올 때 마다,
-		// 그 순서대로 회전 시키기 위해 rotate() 호출.
-		if (cnt == K) {   
-//			rotate(x, y, array);
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		
+		// 원본 배열 할당
+		lst = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				lst[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
+
+		// 회전 연산 정보 할당
+		operation = new int[K][K];
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			r = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			s = Integer.parseInt(st.nextToken());
+			operation[i] = new int[] { r, c, s };
+		}
+
+		/**
+		 * comb() -> deepCopy() -> rotate() -> arrayMin() 순으로 동작
+		 * 회전 연산 순서 정하기 -> 원본 배열을 복사 ->
+		 * 복사본으로 배열 회전 -> 회전이 끝난 후 배열의 최솟값 찾기 & 갱신.
+		 */
+		isSelected = new int[K];
+		comb(1, isSelected);
 		
-		// 중복 순열을 구하는 로직
-		// ...
-		// ...
-		
+		// 최종 출력
+		sb.append(res);
+		System.out.println(sb);
+
+	} // Main
+
+	/**
+	 * 조합 : 회전 연산을 어떤 순서로 돌릴지 정하는 함수
+	 * @param cnt : 현재 선택된 숫자 개수
+	 * @param isSelected : 방문 체크용
+	 */
+	private static void comb(int cnt, int[] isSelected) {
+		if (cnt == K) {
+			// 1. 조합으로 순서가 정해지면, 작업을 위한 원본 배열 복사
+			copyLst = new int[N][M];
+			deepCopy();
+			
+			// 2. 정해진 순서에 의해 수행할 회전 연산 정보 설정
+			for (int num : isSelected) {  // [0, 1], [1, 0]
+				r = operation[num][0];  
+				c = operation[num][1];
+				s = operation[num][2];
+				
+				// 새로 돌려야할 배열의 행, 열
+				nN = Math.abs((r - s - 1) - (r + s - 1)) + 1; 
+				mM = Math.abs((c - s - 1) - (c + s - 1)) + 1; 
+
+				// 한 번 회전시 내부 사각형까지 돌려야 하는 횟수
+				range = Math.min(nN, mM) / 2; // 2
+				visited = new boolean[N][M];
+				
+				// 3. 주어진 회전 연산 정보로 배열 돌리기
+				for (int i = 0; i < range; i++) {
+					startX = r - s - 1 + i;
+					startY = c - s - 1 + i;
+					rotate(startX, startY);
+				}  // for : 1번의 회전연산을 마침.
+			}  // foreach
+			
+			// 4. 1개의 순서로 회전 연산이 모두 끝난 후, 배열의 최솟값 구하기.
+			arrayMin(copyLst); 
+			return;
+		}
+
+		for (int i = 0; i < K; i++) {
+			if (isSelected[i] == 1) {
+				continue;
+			}
+			isSelected[i] = 1;
+			comb(cnt + 1, isSelected);
+			isSelected[i] = 0;
+		}
+	}
+
+	/**
+	 * 원본 배열을 복사하는 함수
+	 */
+	private static void deepCopy() {
+		if (lst == null) {
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			copyLst[i] = Arrays.copyOf(lst[i], M); // 2
+		}
+	}
+
+	/**
+	 * 배열을 돌리는 함수
+	 * @param x : 새로 돌려야 할 배열의 시작 X 좌표
+	 * @param y : 새로 돌려야 할 배열의 시작 Y 좌표
+	 */
+	private static void rotate(int x, int y) {
+		int start = copyLst[x][y];  // 최초 출발점 기억
+		int nx, ny;
+		int direc = 0; // 방향 변수
+
+		while (true) {
+			nx = x + dx[direc];
+			ny = y + dy[direc];
+
+			// 다음 위치가 범위를 벗어나지 않았다면,
+			if (r - s - 1 <= nx && nx < r + s && c - s - 1 <= ny && ny < c + s && !visited[nx][ny]) {
+				// 다음 위치의 값을 현재 위치로 땡겨옴
+				copyLst[x][y] = copyLst[nx][ny];
+				visited[nx][ny] = true;
+				// 현재 위치 -> 다음 위치로 이동
+				x = nx;
+				y = ny;
+
+				// 한 바퀴를 돌고나면, 최초 출발점의 값을 기억한 start변수로
+				// 마지막 위치의 값을 갱신.
+				if (x == startX && y == startY) {
+					copyLst[startX][startY + 1] = start;
+					break;
+				}
+
+			} else {  // 각 방향 끝까지 간 이후 다음 방향으로 전환.
+				direc = (direc + 1) % 4;
+			}
+		} // while
+	} // rotate()
+
+	/**
+	 * 배열의 최솟값을 구하는 함수
+	 * @param arr : 배열의 값을 구할 복사된 배열
+	 */
+	private static void arrayMin(int[][] arr) {
+		int sum = Integer.MAX_VALUE;
+		int temp;
+		for (int[] subArr : arr) {
+			temp = 0;
+			for (int element : subArr) {
+				temp += element;
+			}
+			sum = Math.min(sum, temp);
+		}
+		res = Math.min(res, sum);
 	}
 }
